04ad4f0387ae0636d0fe4491d92f23aee5e2204e
Praveen Agarwal <praveen.agarwal@moveinsync.com>
2025-08-16 12:45:38 +0530

    completed project 3

--- Project3/Project1/DetailViewController.swift
+++ Project3/Project1/DetailViewController.swift
@@ -17,6 +17,7 @@ class DetailViewController: UIViewController {
         
         title = "Picture \(index?.index ?? 0) of \(index?.count ?? 0)"
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         guard let imageToDisplay = selectedImage else { return }
         imageView.image = UIImage(named: imageToDisplay)
@@ -30,5 +31,13 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 1), let imageName = selectedImage else { return }
+        
+        let activityVC = UIActivityViewController(activityItems: [image, imageName], applicationActivities: [])
+        activityVC.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(activityVC, animated: true)
     }
 }
